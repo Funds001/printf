876a098224425fb4869d1bc0098940d73d18529e
Stephen Omoko <jacksonwilson010@gmail.com>
2023-09-18 09:25:35 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
-#include "main.h"
-#include <unistd.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
 
 /**
 * print_char - print a single character
--- main.h
+++ main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 
 int _putchar(char c);
 int _print(const char *format, ...);
+int print_percent(va_list args __attribute__((unused)));
+int print_string(va_list args);
+int print_char(va_list args);
 #endif
